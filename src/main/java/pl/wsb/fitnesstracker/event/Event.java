0927c88b3d53c4027d6cb8ca90d873b54f52f8c0
bartosz.lagoda <composit095@gmail.com>
2025-11-23 11:54:14 +0100

    feat(lab03): finishing database schema for Event

--- src/main/java/pl/wsb/fitnesstracker/event/Event.java
+++ src/main/java/pl/wsb/fitnesstracker/event/Event.java
@@ -1,5 +1,52 @@
 package pl.wsb.fitnesstracker.event;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "Event")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    @Column(name = "startTime", nullable = false)
+    private Date startTime;
+
+    @Column(name = "endTime", nullable = false)
+    private Date endTime;
+
+    @Column
+    private String country;
+
+    @Column
+    private String city;
+
+    @OneToMany(mappedBy = "event", orphanRemoval = true)
+    private List<UserEvent> userEvents = new ArrayList<>();
+
+    public Event(String name, String description, Date startTime, Date endTime, String country, String city) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.country = country;
+        this.city = city;
+    }
 }
