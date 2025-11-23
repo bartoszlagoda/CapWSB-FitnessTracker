2fd30f176ce5f67e6e580b8f213a4525938664ab
bartosz.lagoda <composit095@gmail.com>
2025-11-23 11:53:21 +0100

    feat(lab03): finishing database schema for WorkoutSession

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,17 +1,47 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.training.api.Training;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+@Entity
+@Table(name = "Workout_Session")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class WorkoutSession {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
     private int id;
-    private int trainingId;
+    @ManyToOne
+    @JoinColumn(name = "training_id", referencedColumnName = "id")
+    private Training trainingId;
+    @Column(name = "timestamp")
     private String timestamp;
+    @Column(name = "startLatitude")
     private double startLatitude;
+    @Column(name = "startLongitude")
     private double startLongitude;
+    @Column(name = "endLatitude")
     private double endLatitude;
+    @Column(name = "endLongitude")
     private double endLongitude;
+    @Column(name = "altitude")
     private double altitude;
+
+    public WorkoutSession(Training trainingId, String timestamp, double startLatitude, double startLongitude, double endLatitude, double endLongitude, double altitude) {
+        this.trainingId = trainingId;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
 }
